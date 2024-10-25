ddb3bd99bce18b6757fef2e2885fae937bb0f476
m2gi-subel <lucas.sube@etu.univ-grenoble-alpes.fr>
2024-10-25 10:31:31 +0200

    retour de cartDTO

---
+++ src/main/java/com/je3l/service/dto/CartDTO.java
@@ -0,0 +1,37 @@
+package com.je3l.service.dto;
+
+import com.je3l.domain.Aliment;
+import jakarta.validation.constraints.*;
+import java.io.Serializable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+public class CartDTO implements Serializable {
+
+    //@NotBlank
+    private Aliment aliment;
+
+    //@Size(min = 1)
+    private Integer quantity;
+
+    public CartDTO(Aliment aliment, Integer quantity) {
+        this.aliment = aliment;
+        this.quantity = quantity;
+    }
+
+    public Aliment getAliment() {
+        return aliment;
+    }
+
+    public void setAliment(Aliment aliment) {
+        this.aliment = aliment;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
