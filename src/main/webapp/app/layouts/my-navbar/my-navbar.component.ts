616d33552e375dadb971092fbf0269d952856052
m2gi-bernete <elsa.bernet38@gmail.com>
2024-11-15 15:06:52 +0100

    Passage objet aliment à card product

---
+++ src/main/webapp/app/layouts/my-navbar/my-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-my-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-navbar.component.html',
+  styleUrl: './my-navbar.component.scss'
+})
+export class MyNavbarComponent {
+
+}
