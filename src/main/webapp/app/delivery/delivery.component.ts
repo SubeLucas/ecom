4172b88f29fb7f05fc6f8d2661ed49d731b397ef
m2gi-pottielu <lucas.pottier@etu.univ-grenoble-alpes.fr>
2024-11-29 10:15:43 +0100

    Mise à jour adresse client après saisie infos

--- src/main/webapp/app/delivery/delivery.component.ts
+++ src/main/webapp/app/delivery/delivery.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { AccountService } from 'app/core/auth/account.service';
+import { ClientService } from 'app/entities/client/service/client.service';
 
 @Component({
   selector: 'jhi-delivery',
@@ -12,12 +14,40 @@ import { NgIf } from '@angular/common';
 })
 export class DeliveryComponent {
   private router = inject(Router);
+  private accountService = inject(AccountService);
   street = '';
   code = '';
   city = '';
 
+  constructor(private http: ClientService) {}
+
   onValidateButtonClick(): void {
-    // envoyer infos livraison au backend "street+', '+'code+' '+city" ?
-    this.router.navigate(['payment']);
+    if (this.accountService.isAuthenticated()) {
+      // mise à jour adresse de livraison du client connecté
+      this.http.findCurrent().subscribe({
+        next: client => {
+          if (client.body) {
+            client.body.address = this.street + ', ' + this.code + ' ' + this.city;
+            this.http.update(client.body).subscribe({
+              next: newClient => {
+                console.log(newClient.body);
+              },
+              error: error => {
+                console.error('Erreur lors de la récupération du client courant mis à jour', error);
+              },
+            });
+          } else {
+            console.warn('Pas de client connecté actuellement');
+          }
+        },
+        error: error => {
+          console.error('Erreur lors de la récupération du client courant', error);
+        },
+      });
+
+      this.router.navigate(['payment']);
+    } else {
+      this.router.navigate(['login']);
+    }
   }
 }
