74f40b83fb294ff5c2a56d344e97e4603ec860df
SubeLucas <subelucas.pro@gmail.com>
2024-12-06 10:00:52 +0100

    Back-end prend en compte la date lors de la confirmation d'un panier

--- src/main/java/com/je3l/service/dto/CartDTO.java
+++ src/main/java/com/je3l/service/dto/CartDTO.java
@@ -3,10 +3,13 @@ package com.je3l.service.dto;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.MapSerializer;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class CartDTO implements Serializable {
 
     private CartItem[] cartItems;
+    private int[] deliveryDate;
 
     public CartItem[] getCartItems() {
         return this.cartItems;
@@ -14,5 +17,13 @@ public class CartDTO implements Serializable {
 
     public void setCartItems(CartItem[] cartItems) {
         this.cartItems = cartItems;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return LocalDate.of(deliveryDate[0], deliveryDate[1], deliveryDate[2]);
+    }
+
+    public void setDeliveryDate(int[] date) {
+        deliveryDate = date;
     }
 }
